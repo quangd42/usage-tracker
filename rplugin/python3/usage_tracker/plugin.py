706b6423a259a994a388bb3ba302409279f8ab2a
quangd42 <quangdn42@gmail.com>
2024-10-20 16:17:15 +0700

    remove autocmds on mode change

--- rplugin/python3/usage_tracker/plugin.py
+++ rplugin/python3/usage_tracker/plugin.py
@@ -12,59 +12,41 @@ class LoggerPlugin:
         self._vim = vim
         self._logger: Logger | None = None
         data_dir = vim.funcs.stdpath('data')
-        self._db_path = os.path.join(data_dir, 'utrack-log.db')
+        self._db_path = os.path.join(data_dir, 'utracker-log.db')
 
-    @pynvim.command('UTrackerRun', nargs='*', sync=True)
-    def start_logger(self, args):
-        try:
+    @pynvim.command('UTrackerRun', nargs='0', sync=True)
+    def start_logger(self) -> None:
+        if self._logger is None:
             self._logger = Logger(DatabaseQueries(self._db_path), 'nvim')
+        try:
             self._logger.start()
             self._vim.api.notify('Logger started', 2, {})
         except Exception as e:
             self._vim.api.notify(f'Logger failed: {e}', 4, {})
 
-    @pynvim.command('UTrackerStop', nargs='*', sync=True)
-    def stop_logger(self, args):
-        if self._logger and self._logger.running:
-            self._logger.stop()
-            self._vim.api.notify('Logger stopped', 2, {})
+    @pynvim.command('UTrackerStop', nargs='0', sync=True)
+    def stop_logger(self) -> None:
+        if not self._logger or not self._logger.running:
+            return
+        self._logger.stop()
+        self._vim.api.notify('Logger stopped', 2, {})
+
+    @pynvim.command('UTrackerPause', nargs='0', sync=True)
+    def pause_logger(self) -> None:
+        if not self._logger or not self._logger.running or self._logger.is_paused:
+            return
+        self._logger.pause()
+        self._vim.api.notify('Logger paused', 2, {})
+
+    @pynvim.command('UTrackerResume', nargs='0', sync=True)
+    def resume_logger(self) -> None:
+        if not self._logger or self._logger.running or not self._logger.is_paused:
+            return
+        self._logger.resume()
+        self._vim.api.notify('Logger resumed', 2, {})
 
     @pynvim.autocmd('VimLeavePre', pattern='*')
-    def vim_leave_handler(self):
-        if self._logger:
-            self._logger.stop()
-
-    @pynvim.autocmd('InsertEnter', pattern='*', sync=False)
-    def insert_enter_handler(self):
-        if self._logger:
-            self._logger.pause()
-            self._vim.api.notify('Logger paused', 2, {})
-
-    @pynvim.autocmd('InsertLeave', pattern='*', sync=False)
-    def insert_leave_handler(self):
-        if self._logger and self._logger.is_paused:
-            self._logger.resume()
-            self._vim.api.notify('Logger resumed', 2, {})
-
-    @pynvim.autocmd('BufEnter', pattern='*', sync=False)
-    def buf_enter_handler(self):
-        if self._logger and self._logger.is_paused:
-            self._logger.resume()
-            self._vim.api.notify('Logger resumed', 2, {})
-
-    @pynvim.autocmd('FocusLost', pattern='*', sync=False)
-    def vim_focus_lost_handler(self):
-        if self._logger and not self._logger.is_paused:
-            self._logger.pause()
-            self._vim.api.notify('Logger paused', 2, {})
-
-    @pynvim.autocmd('FocusGained', pattern='*', sync=False)
-    def vim_focus_gained_handler(self):
-        if self._logger:
-            insert_mode = self._vim.api.get_mode()['mode'] == 'i'
-            if insert_mode and not self._logger.is_paused:
-                self._logger.pause()
-                self._vim.api.notify('Logger paused', 2, {})
-            elif not insert_mode and self._logger.is_paused:
-                self._logger.resume()
-                self._vim.api.notify('Logger resumed', 2, {})
+    def vim_leave_handler(self) -> None:
+        if not self._logger or not self._logger.running:
+            return
+        self._logger.stop()
